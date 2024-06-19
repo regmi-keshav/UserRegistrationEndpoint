f4ab89974a3170be6c6b51ba4475f621d2a06b8c
keshav_regmi <keshavregmi70@gmail.com>
2024-06-20 01:25:09 +0530

    Initial Commit

---
+++ src/main/java/com/example/UserRegistrationEndpoint/UserRegistrationEndpointApplication.java
@@ -0,0 +1,13 @@
+package com.example.UserRegistrationEndpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserRegistrationEndpointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserRegistrationEndpointApplication.class, args);
+	}
+
+}
